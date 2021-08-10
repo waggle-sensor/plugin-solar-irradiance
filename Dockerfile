96ca02b347564540569dd0f12816bc5e26a3e51f
Yongho Kim <ironyonghokim@gmail.com>
2021-08-10 14:11:55 -0500

    Fixed bugs; Added required Python libraries

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,7 @@
 FROM waggle/plugin-base:1.1.1-ml-cuda10.2-l4t
 
-COPY requirements.txt /app/
+COPY app.py requirements.txt /app/
 RUN pip3 install --no-cache-dir -r /app/requirements.txt
-
-COPY app.py /app/
-
-ARG SAGE_STORE_URL="https://osn.sagecontinuum.org"
-
-ENV SAGE_STORE_URL=${SAGE_STORE_URL}
 
 WORKDIR /app
 ENTRYPOINT ["python3", "-u", "/app/app.py"]
--- app.py
+++ app.py
@@ -17,16 +17,17 @@ plugin.init()
 plugin.subscribe(TOPIC_CLOUDCOVER)
 
 class cal_max_irr:
-    def __init__(self):
+    def __init__(self, geo_location):
         self.maxirrs = {}
         self.lastupdate = '2020-06-20'
+        self.geo_location = geo_location
 
     def cal(self, timestamp):
         if datetime.datetime.fromtimestamp(timestamp).date() != self.lastupdate:
             self.solarpy(datetime.datetime.fromtimestamp(timestamp).date())
 
         timestamp_low = datetime.datetime.fromtimestamp(timestamp).time()
-        timestamp_high = (datetime.datetime.fromtimestamp(timestamp)+datetime.timedelta(seconds=args.interval)).time()
+        timestamp_high = (datetime.datetime.fromtimestamp(timestamp)+datetime.timedelta(seconds=60)).time()
 
         for k, v in self.maxirrs['GHI'].items():
             dt = datetime.datetime.strptime(k, '%Y-%m-%dT%H:%M:%S').time()
@@ -35,9 +36,8 @@ class cal_max_irr:
 
     def solarpy(self, date):
         self.lastupdate = date
-        # Lamont, Oklahoma
-        tz = 'MST'
-        lat, lon = 36.691959, -97.565965
+        tz = 'UTC'
+        lat, lon = self.geo_location
         site = location.Location(lat, lon, tz=tz)
 
         def get_irradiance(site_location, date, tilt, surface_azimuth):
@@ -50,13 +50,16 @@ class cal_max_irr:
         self.maxirrs.index = self.maxirrs.index.strftime('%Y-%m-%dT%H:%M:%S')
         self.maxirrs = self.maxirrs.to_dict('dict')
 
-        
 
-def run():
-    maxirr = cal_max_irr()
+def run(args):
+    print('Generating solar irradiance table...')
+    # The default location (36.691959, -97.565965) is Lamont, Oklahoma
+    maxirr = cal_max_irr(geo_location=(args.node_latitude, args.node_longitude))
     maxirr.solarpy(datetime.datetime.fromtimestamp(time.time()).date())
 
+    print('Solar Irradiance estimator starts...')
     while True:
+        print('Getting cloud coverage ratio...')
         ratio = plugin.get()
         rvalue = ratio.value
         timestamp = ratio.timestamp
@@ -64,18 +67,21 @@ def run():
 
         irr = (1-rvalue) * current_max_irr
         plugin.publish(TOPIC_SOLARCLOUD, irr, timestamp=timestamp)
-        if args.debug:
-            print(f"Measures published: Solar irradiance = {irr}")
+        print(f"Measures published: Solar irradiance = {irr}")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-        parser.add_argument(
+    parser.add_argument(
         '-debug', dest='debug',
         action='store_true', default=False,
         help='Debug flag')
     parser.add_argument(
-        '-interval', dest='interval',
-        action='store', default=0, type=int,
-        help='Inference interval in seconds')
+        '-node-latitude', dest='latitude', type=float,
+        default=36.691959,
+        help='latitude of the node location')
+    parser.add_argument(
+        '-node-longitude', dest='longitude', type=float,
+        default=-97.565965,
+        help='longitude of the node location')
     run(parser.parse_args())
